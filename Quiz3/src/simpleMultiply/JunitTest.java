c67bd2115c62c9b49d23c9d40134103e3d8cdb20
Delowar Hossen Apple <applecse17@gmail.com>
2018-11-21 10:03:10 +0600

    Quiz 3 programm complete with JunitTest

--- Quiz3/src/simpleMultiply/JunitTest.java
+++ Quiz3/src/simpleMultiply/JunitTest.java
@@ -4,11 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class JunitTest {
+class TestTest {
+	private static final double TIME_INTERVAL = 7.2;
+	private static final double SPEED = 1100;
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		Sound sc = new Sound();
+		assertEquals(7920.0,sc.calculation(TIME_INTERVAL,SPEED)," ");
+
 	}
 
 }
